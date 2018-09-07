746df1d8b42548ab4f8dfa872114de5e84e76d05
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2018-09-07 14:23:56 +0200

    Create a about page

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,5 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='pages/home_page.html'), name='home-page'),
+    path('about/', TemplateView.as_view(template_name='pages/about_page.html'), name='about-page'),
 ]
