976b69622cb7fe0a794d138661e7d7db6edde30f
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2018-09-07 14:31:33 +0200

    Create a contact page

--- pages/urls.py
+++ pages/urls.py
@@ -7,4 +7,5 @@ app_name = 'pages'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='pages/home_page.html'), name='home-page'),
     path('about/', TemplateView.as_view(template_name='pages/about_page.html'), name='about-page'),
+    path('contact/', TemplateView.as_view(template_name='pages/contact_page.html'), name='contact-page'),
 ]
