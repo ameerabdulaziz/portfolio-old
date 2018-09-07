ef7b68404f6595f137c6a901adcf4cbb4e8f81b9
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2018-09-07 15:08:09 +0200

    Create a project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    image = models.ImageField('projects')
+    preview_link = models.URLField()
+    github_link = models.URLField()
+    added = models.DateTimeField(auto_now_add=datetime)
+
+    def __str__(self):
+        return self.title
